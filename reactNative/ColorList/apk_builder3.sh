ae59d800acc53221e83d6e0b9b88f78c4f33cbe6
ankith86 <55974015+ankith86@users.noreply.github.com>
2020-02-17 23:54:46 +0530

    test1

---
+++ reactNative/ColorList/apk_builder3.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "Please provide the path to directory, where you want to run this script ..?";
+location=$1
+DIR="$location/android/app/src/main/assets"
+
+function apk_Builder(){
+	 cd $location/
+         echo " Working directory is $PWD "
+
+         npx react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res
+
+         echo "Android directory "
+         cd $location/android/
+         echo " Working directory is $PWD "
+
+         echo "Gradlew Process starts here "
+
+         ./gradlew assembleDebug
+
+         echo " ends here"
+
+         exit 0
+
+}
+
+if [ -d "$DIR" ]; then
+  echo "Directory ${DIR} exists and proceeding further"
+  apk_Builder
+else
+
+  echo "The ${DIR} not found. So creating directory ${DIR}"
+  path=$location/android/app/src/main
+
+  cd $path/
+  mkdir assets
+
+  if [ -d "$DIR" ]; then
+  echo "Directory ${DIR} now exists and proceeding further"
+  fi
+
+  echo "Directory ${DIR} has been created and proceeding further"
+  apk_Builder
+fi
+
+
